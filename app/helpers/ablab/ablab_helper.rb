8af130ff1246dd7d483cf390a5bc32c709fb0066
Luca Ongaro <lukeongaro@gmail.com>
2016-02-12 17:52:57 +0100

    better visualization of results

--- app/helpers/ablab/ablab_helper.rb
+++ app/helpers/ablab/ablab_helper.rb
@@ -1,18 +1,18 @@
 module Ablab
   module AblabHelper
-    def format_ratio(numerator, denominator, na = 'n/a')
+    def format_percentage(numerator, denominator, na = 'n/a')
       return na if denominator == 0
-      '%.3f' % (numerator.to_f / denominator)
+      number_to_percentage((numerator.to_f / denominator) * 100.0)
     end
 
     def format_number(number, na = 'n/a')
       return na if number.nil? || number.try(:nan?)
-      number_with_delimiter(number, separator: '.', delimiter: ',')
+      number_with_delimiter(number)
     end
 
     def format_decimal(decimal, na = 'n/a')
       return na if decimal.nil? || decimal.nan?
-      '%.3f' % decimal
+      number_with_precision(decimal, precision: 3)
     end
 
     def significant?(results)
@@ -22,6 +22,7 @@ module Ablab
 
     def confidence(z_score, na = 'n/a')
       return na if z_score.nil? || z_score.nan?
+      z_score = z_score.abs
       if z_score >= 2.58
         '99%'
       elsif z_score >= 1.96
@@ -33,7 +34,21 @@ module Ablab
       end
     end
 
+    def cr_gain(results_a, results_b, na = 'n/a')
+      return na if results_a[:sessions] == 0 || results_b[:sessions] == 0
+      cr_a = results_a[:conversions].to_f / results_a[:sessions]
+      cr_b = results_b[:conversions].to_f / results_b[:sessions]
+      gain = cr_a - cr_b
+      "#{gain >= 0 ? '+' : ''}#{number_to_percentage(gain * 100.0)}"
+    end
+
     def winner?(experiment, group_name)
+      winner(experiment) == group_name
+    end
+
+    def winner(experiment)
+      @winners ||= {}
+      return @winners[experiment.name] if @winners.has_key?(experiment.name)
       winner_name, winner_results = experiment.results.max_by do |(_, r)|
         if r[:sessions] > 0
           r[:conversions].to_f / r[:sessions]
@@ -41,7 +56,21 @@ module Ablab
           -1
         end
       end
-      significant?(winner_results) && winner_name == group_name
+      @winners[experiment.name] = nil
+      @winners[experiment.name] = winner_name if significant?(winner_results)
+      @winners[experiment.name]
+    end
+
+    def gains(experiment, group_name)
+      result = experiment.results[group_name]
+      return nil if result[:sessions] == 0
+      cr = result[:conversions].to_f / result[:sessions]
+      experiment.results.reduce({}) do |hash, (g, r)|
+        if r[:sessions] > 0 && g != group_name
+          hash[g] = cr - (r[:conversions].to_f / r[:sessions])
+        end
+        hash
+      end
     end
   end
 end
